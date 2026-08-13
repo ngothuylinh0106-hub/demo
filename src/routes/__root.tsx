418544d1fa1998e2885276e71b87bbd42d83edd0
ngothuylinh0106-hub <ngothuylinh0106@gmail.com>
2026-08-13 10:22:27 +0700

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -100,7 +100,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: "https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@400;500;600;700;800&display=swap",
       },
-      { rel: "icon", href: "/favicon-2.png", type: "image/x-icon" },
+      { rel: "icon", href: "public/favicon-2.png", type: "image/x-icon" },
     ],
     scripts: [
       {
