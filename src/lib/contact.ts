08dd471b1b2c7c15c37ac3892c67feed998a023e
ngothuylinh0106-hub <ngothuylinh0106@gmail.com>
2026-08-16 18:53:32 +0700

    deloy 2

--- src/lib/contact.ts
+++ src/lib/contact.ts
@@ -14,12 +14,7 @@ export const sendContactEmail = createServerFn({
 })
   .validator(contactServerSchema)
   .handler(async ({ data }) => {
-    const apiKey = 
-  (globalThis as any).RESEND_API_KEY || 
-  process.env['RESEND_API_KEY'] || 
-  (globalThis as any).process?.env?.RESEND_API_KEY;
-
-
+    const apiKey = (globalThis as any).RESEND_API_KEY || process.env['RESEND_API_KEY'];
 
     const resend = new Resend(apiKey);
 
