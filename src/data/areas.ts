7ee535456664e8f7ad2a5547b8c786d2e3a7fbd0
ngothuylinh0106-hub <ngothuylinh0106@gmail.com>
2026-08-16 10:14:20 +0700

    Update areas.ts

--- src/data/areas.ts
+++ src/data/areas.ts
@@ -5,14 +5,44 @@ export interface Area {
 }
 
 export const areas: Area[] = [
-  { slug: "Bình Trị Đông A", name: "Bình Trị Đông A", note: "Bình Tân" },
-  { slug: "Bình Trị Đông B", name: "Bình Trị Đông B", note: "Bình Tân" },
-  { slug: "Tân Tạo A", name: "Đà Nẵng", note: "Bình Tân" },
-  { slug: "Tân Tạo B", name: "Bình Dương", note: "Bình Tân" },
-  { slug: "Bình Tân", name: "Đồng Nai", note: "Bình Tân" },
-  { slug: "Bình Hưng Hòa A", name: "Hải Phòng", note: "Bình Tân" },
-  { slug: "Bình Hưng Hòa B", name: "Bình Hưng Hòa B", note: "Bình Tân" },
-  { slug: "ba-ria-vung-tau", name: "Hoag ", note: "Bình Tân" },
-  { slug: "long-an", name: "Long An", note: "Bình Tân" },
-  { slug: "bac-ninh", name: "Bắc Ninh", note: "Bình Tân" },
+  {
+    slug: "binh-tri-dong-a",
+    name: "Bình Trị Đông A",
+    note: "Bình Tân",
+  },
+  {
+    slug: "binh-tri-dong-b",
+    name: "Bình Trị Đông B",
+    note: "Bình Tân",
+  },
+  {
+    slug: "tan-tao-a",
+    name: "Tân Tạo A",
+    note: "Bình Tân",
+  },
+  {
+    slug: "tan-tao-b",
+    name: "Tân Tạo B",
+    note: "Bình Tân",
+  },
+  {
+    slug: "binh-hung-hoa-a",
+    name: "Bình Hưng Hòa A",
+    note: "Bình Tân",
+  },
+  {
+    slug: "binh-hung-hoa-b",
+    name: "Bình Hưng Hòa B",
+    note: "Bình Tân",
+  },
+  {
+    slug: "binh-hung-hoa",
+    name: "Bình Hưng Hòa",
+    note: "Bình Tân",
+  },
+  {
+    slug: "binh-tan",
+    name: "Bình Tân",
+    note: "TP. Hồ Chí Minh",
+  },
 ];
